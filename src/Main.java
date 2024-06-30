77e159bc955d507edf27547c115849192a20efd9
Fasakhova <kvisti2009@mail.ru>
2024-06-30 21:07:18 +0300

    In commit

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        int ticket = 13676; // цена билета
+        int ticket = 15524; // цена билета
         int mile = 20; // 1 миля = 20 руб
         int bonus = ticket / mile;
         System.out.println("Количество начисленных миль:" + bonus);
