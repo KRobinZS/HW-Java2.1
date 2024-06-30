6b08934e65ecc06ccd3959f988fca0598644c274
Fasakhova <kvisti2009@mail.ru>
2024-06-30 20:56:02 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int ticket = 13676; // цена билета
+        int mile = 20; // 1 миля = 20 руб
+        int bonus = ticket / mile;
+        System.out.println("Количество начисленных миль:" + bonus);
+        }
+    }
