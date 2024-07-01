34de520018dc08391e95cdc1f0bfbf21d7467183
Fasakhova <kvisti2009@mail.ru>
2024-07-01 14:48:10 +0300

    Two commit

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,9 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        int ticket = 15524; // цена билета
-        int mile = 20; // 1 миля = 20 руб
-        int bonus = ticket / mile;
+        int Ticket = 15524; // цена билета
+        int Mile = 20; // 1 миля = 20 руб
+        int bonus = Ticket / Mile;
         System.out.println("Количество начисленных миль:" + bonus);
-        }
     }
+}
